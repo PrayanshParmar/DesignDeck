c0bed9a0c1906c4ffd8484b9f199bf5c99b34ec9
prayansh <parmarprayansh2@gmail.com>
2024-10-10 17:02:38 +0530

    Updated route for guest (#52)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,13 +13,14 @@ import { Button } from "./ui/button";
 import ShapesMenu from "./ShapesMenu";
 import ThemeSwitcher from "./ui/ThemeSwitcher";
 import { useTheme } from "next-themes";
+import { useAuth } from '@clerk/nextjs'
 import { UserButton } from "@clerk/nextjs";
 
-function Navbar({
+async function Navbar({
   activeElement,
   imageInputRef,
   handleImageUpload,
-  handleActiveElement,
+  handleActiveElement
 }: NavbarProps) {
   const isActive = (value: string | Array<ActiveElement>) =>
     (activeElement && activeElement.value === value) ||
@@ -29,6 +30,8 @@ function Navbar({
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "dark" ? systemTheme : theme;
   const [darkMode, setDarkMode] = useState(false);
+  const { userId} = useAuth()
+
   useEffect(() => {
     if (currentTheme === "dark") {
       setDarkMode(true);
@@ -37,20 +40,9 @@ function Navbar({
     }
   }, [currentTheme]);
 
-  // console.log(themeCheck)
-  console.log(darkMode);
-
   return (
-    <div
-      className={`${
-        darkMode
-          ? "bg-primary-black text-white"
-          : "bg-white text-black border-b-0 border-black"
-      }`}
-    >
-      <nav
-        className={`flex select-none items-center justify-between gap-4  px-5`}
-      >
+    <div className={`${darkMode ? "bg-primary-black text-white" : "bg-white text-black border-b-0 border-black"}`}>
+      <nav className="flex select-none items-center justify-between gap-4 px-5">
         <Link href="/">
           <Image
             src="/assets/logo.png"
@@ -71,14 +63,14 @@ function Navbar({
                   handleActiveElement(item);
                 }}
                 className={`group px-2.5 py-5 flex justify-center items-center
-          ${
-            isActive(item.value)
-              ? "bg-primary-green"
-              : "hover:bg-primary-grey-200"
-          }
-          `}
+                  ${
+                    isActive(item.value)
+                      ? "bg-primary-green"
+                      : "hover:bg-primary-customGray"
+                  }
+                `}
               >
-                {Array.isArray(item.value) ? ( // Renders shapes menu
+                {Array.isArray(item.value) ? (
                   <ShapesMenu
                     item={item}
                     activeElement={activeElement}
@@ -86,25 +78,23 @@ function Navbar({
                     handleActiveElement={handleActiveElement}
                     handleImageUpload={handleImageUpload}
                   />
-                ) : item.value === "comments" ? ( // Renders comments cursor
+                ) : item.value === "comments" ? (
                   <NewThread>
                     <Button className="relative w-5 h-5 object-contain">
-                      <Image
-                        src={item.icon}
-                        alt={item.name}
-                        fill
-                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 20vw"
-                        className={isActive(item.value) ? "invert" : ""}
-                      />
+                    <Image
+                      src={item.icon}
+                      alt={item.name}
+                      fill
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 20vw"
+                      className={`${darkMode ? "filter-white" : "filter-black"}`}
+                    />
+
                     </Button>
                   </NewThread>
                 ) : (
-                  // Renders other nav elements
                   <Button
-                    className={`relative w-5 h-5 object-contain p-2 items-center justify-center  focus:outline-none focus:ring-0 ${
-                      darkMode
-                        ? "border-gray-300 text-white  "
-                        : " text-black  "
+                    className={`relative w-5 h-5 object-contain p-2 items-center justify-center focus:outline-none focus:ring-0 ${
+                      darkMode ? "border-gray-300 text-white" : "text-black"
                     }`}
                   >
                     <Image
@@ -112,7 +102,7 @@ function Navbar({
                       alt={item.name}
                       fill
                       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 20vw"
-                      className={isActive(item.value) ? "invert" : ""}
+                      className={`${darkMode ? "invert" : ""}`} // Change icon color based on dark mode
                     />
                   </Button>
                 )}
@@ -121,19 +111,11 @@ function Navbar({
           </ul>
         </div>
 
-        <div
-          className={`flex space-x-4 ${darkMode ? "text-white" : "text-black"}`}
-        >
-          <Link
-            href="/"
-            className="hover:underline  transition duration-300 ease-in-out"
-          >
+        <div className={`flex space-x-4 ${darkMode ? "text-white" : "text-black"}`}>
+          <Link href="/" className="hover:underline transition duration-300 ease-in-out">
             Home
           </Link>
-          <Link
-            href="/faq"
-            className="hover:underline  transition duration-300 ease-in-out"
-          >
+          <Link href="/faq" className="hover:underline transition duration-300 ease-in-out">
             FAQ
           </Link>
         </div>
@@ -141,15 +123,10 @@ function Navbar({
           <ThemeSwitcher />
         </div>
 
-        {/* <ActiveUsers /> */}
-        {/* Added Clerk user profile button */}
-        <UserButton />
+        { userId ? <UserButton/> :<ActiveUsers/> }
       </nav>
     </div>
   );
 }
 
-export default memo(
-  Navbar,
-  (prevProps, nextProps) => prevProps.activeElement === nextProps.activeElement
-);
+export default memo(Navbar, (prevProps, nextProps) => prevProps.activeElement === nextProps.activeElement);
